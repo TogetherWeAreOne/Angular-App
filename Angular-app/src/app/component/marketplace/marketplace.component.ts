5bca86a2411b37dd4a70b5b290c12b41329ba7f4
Ad <adrienroux94@gmail.com>
2021-07-19 20:52:55 +0200

    MarketPlace

--- Angular-app/src/app/component/marketplace/marketplace.component.ts
+++ Angular-app/src/app/component/marketplace/marketplace.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit} from '@angular/core';
 })
 export class MarketplaceComponent implements OnInit {
 
+  public items = [
+    {
+      name: "Manette de jeu",
+      price: 13 + "€",
+      category: "Catégorie",
+      cityAndPostalCode: 75001 + " " + "Paris 1er Arrondissement",
+      imageLink: "/assets/controller-image.jpg"
+    },
+    {
+      name: "Bouteille",
+      price: 15 + "€",
+      category: "Catégorie",
+      cityAndPostalCode: 75001 + " " +  "Paris 1er Arrondissement",
+      imageLink: "/assets/bottle.jpg"
+    },
+    {
+      name: "Stylo",
+      price: 30 + "€",
+      category: "Catégorie",
+      cityAndPostalCode: 75001 + " " +  "Paris 1er Arrondissement",
+      imageLink: "/assets/pen.jpg"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
